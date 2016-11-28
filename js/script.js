d1751d6c881d18305fa1285b4ba43bccdcad066f
moniac <zCNy1LC!&c5j>
2016-11-28 18:41:46 +0100

    final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var changePlus = function () {
-  var plus = document.querySelector(".plus"),
-      filter = document.querySelector(".filter");
+  var plus = document.querySelector("body>section span"),
+      filter = document.querySelector("body>section>section");
       plus.addEventListener("click", function () {
         filter.classList.toggle("geen-display");
       });
@@ -9,10 +9,11 @@ var changePlus = function () {
 changePlus();
 
 plusToVinkje = function () {
-  var plusje = document.querySelector(".plusje");
+  var plusje = document.querySelector("body section ul li section span");
    plusje.addEventListener('click', function () {
     if (plusje.innerHTML === "+") {
-      plusje.innerHTML = "+✓";
+      plusje.innerHTML = "+&#10003";
+
     }
     else {
       plusje.innerHTML = "+";
@@ -21,22 +22,59 @@ plusToVinkje = function () {
 };
 plusToVinkje();
 
-var image = document.querySelectorAll("body section ul li section img");
-//var image_tracker = "nietopgeslagen";
+// var image = document.querySelectorAll("body section ul li section img");
+//
+//
+// var change = function (that) {
+//         if(that.getAttribute("src") == "/img/empty.png"){
+//             that.src = "img/fill.png";
+//
+//         }
+//         else{
+//             that.src = "/img/empty.png";
+//
+//         }
+// }
+//
+// for (var i = 0; i < image.length; i++){
+//     image[i].addEventListener("click", function(){
+//         change(this);
+//     });
+// }
 
-var change = function (that) {
-        if(that.getAttribute("src") == "/img/empty.png"){
-            that.src = "img/fill.png";
-//            image_tracker = "opgeslagen";
-        }
-        else{
-            that.src = "/img/empty.png";
-//            image_tracker = "nietopgeslagen";
-        }
-}
+var changeDoof = function () {
+  var section = document.querySelector(".paranoia"),
+      filter = document.querySelector("body"),
+      doof = document.querySelector(".doof"),
+      dialoog = document.querySelector(".dialoog");
+      section.addEventListener("mouseenter", function () {
+        filter.classList.toggle("paranoia");
+      });
+section.addEventListener("mouseleave", function () {
+  filter.classList.toggle("paranoia");
+});
+doof.addEventListener("mouseenter", function () {
+  filter.classList.toggle("doof");
+});
+doof.addEventListener("mouseleave", function () {
+  filter.classList.toggle("doof");
+});
+dialoog.addEventListener("mouseenter", function () {
+  filter.classList.toggle("dialoog");
+});
+dialoog.addEventListener("mouseleave", function () {
+  filter.classList.toggle("dialoog");
+});
+};
 
-for (var i = 0; i < image.length; i++){
-    image[i].addEventListener("click", function(){
-        change(this);
-    });
-}
+changeDoof();
+
+var labelOpen = function () {
+  var box = document.querySelector('.heart'),
+  label = document.querySelector('.label');
+  box.addEventListener("click", function () {
+  label.classList.add("open");
+  });
+};
+
+labelOpen();
